61dbe605a33687042ef1d3fb649cc385bd635eec
Adrian Cortes <ls-adrian@hotmail.com>
2025-07-17 21:56:16 -0500

    Se implemento github pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: 'https://github.com/Adrian-Corttes/Aventura_Mundo',
+  
 })
